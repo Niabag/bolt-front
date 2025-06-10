0e63675408389ac003b812a41c3ccaaa8add458b
Niabag <gabain@hotmail.fr>
2025-06-10 07:01:53 +0200

    ff

---
+++ Frontend/src/pages/Tutorials/Index.jsx
@@ -0,0 +1,15 @@
+import '../Placeholder/placeholder.scss';
+
+const Tutorials = () => (
+  <div className="placeholder-page">
+    <h1>Tutoriels</h1>
+    <p>Apprenez pas à pas à maîtriser toutes les facettes de CRM Pro.</p>
+    <ul>
+      <li>Premiers pas</li>
+      <li>Personnalisation de votre compte</li>
+      <li>Automatisation des tâches</li>
+    </ul>
+  </div>
+);
+
+export default Tutorials;
